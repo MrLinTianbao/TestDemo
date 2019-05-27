9dfa239d3e072f27662ca997fef0e552c2990992
DUONIU_MAC <1223961777@qq.com>
2019-05-27 11:22:14 +0800

    创建项目

---
+++ TestViewDemo/Extend.swift
@@ -0,0 +1,16 @@
+//
+//  Extend.swift
+//  TestViewDemo
+//
+//  Created by DUONIU_MAC on 2019/5/25.
+//  Copyright © 2019年 DUONIU_MAC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Extend : UILabel {
+    
+    
+}
---
+++ TestViewDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestViewDemo
+//
+//  Created by DUONIU_MAC on 2019/5/25.
+//  Copyright © 2019年 DUONIU_MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+
+
+}
+
